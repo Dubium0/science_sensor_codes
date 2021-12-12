da80d5b0336e8f31840b80e0450b263a8e2d998f
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2021-12-12 17:03:32 +0300

    Update bmp180.ino

--- bmp180.ino
+++ bmp180.ino
@@ -11,45 +11,16 @@ std_msgs::String basinc_msg;
 
 ros::Publisher Sensors("SENSORS",&basinc_msg);
 void setup() {
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-  while (1) {}
-  }
+  bmp.begin(); 
   nh.initNode();
   nh.advertise(Sensors);
 }
   
 void loop() {
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
-
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(seaLevelPressure_hPa * 100));
-    Serial.println(" meters");
-    
-    Serial.println();
-
     String temperature =  String(bmp.readTemperature());
     String pressure =  String(bmp.readPressure());
     String altitude =  String(bmp.readAltitude());
     String alldatas = temperature + "," + pressure + "," + altitude;
-    Serial.print("Alldatas = ");
-    Serial.print(alldatas);
-    Serial.println();
 
     basinc_msg.data = alldatas.c_str();
     Sensors.publish( &basinc_msg );
